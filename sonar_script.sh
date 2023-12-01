d71683f27ad82378e4fc8b2a6b3c14d64bf5fca2
VISHALMACOM <vishalmacom3@gmail.com>
2023-11-30 19:16:57 -0500

    Create sonar_script.sh

---
+++ sonar_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Pattern to search for processes
+pattern="java"  # Replace 'your_pattern_here' with the actual pattern
+
+# Use pgrep to find process IDs (PIDs) matching the pattern
+pids=$(pgrep "$pattern")
+
+if [ -z "$pids" ]; then
+    echo "No processes found matching the pattern '$pattern'"
+else
+    echo "Processes found matching the pattern '$pattern':"
+    echo "$pids"
+    # Kill the processes
+    pkill "$pattern"
+    echo "Processes killed."
+fi
+
+
+cmd="./sonarqube-9.9.0.65466/bin/linux-x86-64/sonar.sh console"
+nohup $cmd > /dev/null 2>&1 &
