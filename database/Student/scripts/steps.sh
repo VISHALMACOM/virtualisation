47def530c7a3a35ea1f1b136dcd754106d168137
VISHALMACOM <vishalmacom3@gmail.com>
2023-12-01 22:47:54 -0500

    Update steps.sh

--- database/Student/scripts/steps.sh
+++ database/Student/scripts/steps.sh
@@ -1,6 +1,7 @@
-l_first_cmd="python ./Student/manage.py makemigrations"
+l_first_cmd="wait-for-it.sh db:3306 -- python manage.py makemigrations"
 l_sec_cmd="python ./Student/manage.py migrate"
 l_third_cmd="python ./Student/manage.py runserver 0.0.0.0:8000"
-sh $l_first_cmd
-sh $l_sec_cmd
-sh $l_third_cmd
+$l_first_cmd
+$l_sec_cmd
+$l_third_cmd
+
